5f220f6171db75274280b106d43f9513d964a6a0
Elvis <elvis.wachira@student.moringaschool.com>
2025-03-24 11:37:03 +0300

    new character

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 // Your code here
+https://flata-hpxq.vercel.app/characters 
+
+fetch('https://flata-hpxq.vercel.app/characters')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  GET /characters
+  [
+   {
+     "id": 1,
+     "name": "Mr. Cute",
+     "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
+     "votes": 0
+   },
+   {
+     "id": 2,
+     "name": "Mr. Monkey",
+     "image": "https://thumbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif",
+     "votes": 0
+   },
+   {
+        "id": 3,
+        "name": "Mr. Grumpy",
+        "image": "https://thumbs.gfycat.com/UnselfishSaneHornedtoad-max-1mb.gif",
+        "votes": 0
+   }
+   {
+        "id": 4,
+        "name": "Mr. Sleepy",
+        "image": "https://thumbs.gfycat.com/UnselfishSaneHornedtoad-max-1mb.gif",
+        "votes
+   }
+   
+  ]
+
+   
